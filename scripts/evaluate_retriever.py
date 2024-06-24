81690ac97988443cb04206e640b8e5c88c758176
Hugues <hsibille@student.ethz.ch>
2024-06-24 13:53:27 +0000

    feat(scripts): support collection evaluation

--- scripts/evaluate_retriever.py
+++ scripts/evaluate_retriever.py
@@ -7,16 +7,19 @@ from dotenv import load_dotenv
 from vidore_benchmark.evaluation.evaluate import evaluate_dataset
 from vidore_benchmark.retrievers.utils.initialize_retrievers import load_vision_retriever_from_registry
 from vidore_benchmark.utils.constants import OUTPUT_DIR
+from vidore_benchmark.evaluation.evaluate import evaluate_dataset
+from vidore_benchmark.utils.log_utils import log_metrics
+import huggingface_hub
 
 load_dotenv(override=True)
 
-
 def main(
     model_name: Annotated[str, typer.Option(help="Model name to use for evaluation")],
-    dataset_name: Annotated[str, typer.Option(help="Dataset on Hugging Face to evaluate")],
     split: Annotated[str, typer.Option(help="Split to use for evaluation")],
+    dataset_name: Annotated[str, typer.Option(help="Dataset on Hugging Face to evaluate")] = "",
     batch_query: Annotated[int, typer.Option(help="Batch size to use for evaluation")] = 1,
     batch_doc: Annotated[int, typer.Option(help="Batch size to use for evaluation")] = 4,
+    collection_name: Annotated[str, typer.Option(help="Collection name to use for evaluation")] = "",
 ):
     """
     This script is used to evaluate a model on a given dataset using the MTEB metrics.
@@ -26,19 +29,30 @@ def main(
     retriever = load_vision_retriever_from_registry(model_name)
 
     # Load the dataset
-    dataset = cast(Dataset, load_dataset(dataset_name, split=split))
-
-    # Evaluate the model
-    metrics = evaluate_dataset(retriever, dataset, batch_query=batch_query, batch_doc=batch_doc)
-
-    # Save the metrics as a JSON file
     OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
     savepath = OUTPUT_DIR / f"{model_name.replace('/', '_')}_metrics.json"
-    with open(savepath, "w", encoding="utf-8") as f:
-        json.dump(metrics, f)
-    print(f"Metrics saved to `{savepath}`")
+    metrics = {}
 
+    if dataset_name != "":
+        dataset = cast(Dataset, load_dataset(dataset_name, split=split))
+        metrics = evaluate_dataset(retriever, dataset, batch_query= batch_query, batch_doc=batch_doc)
+        log_metrics(metrics, dataset_name, log_file=str(savepath))
+        print(f"NDCG@5 for {model_name} on {dataset_name}: {metrics['ndcg_at_5']}")
+
+    elif collection_name != "":
+        collection = huggingface_hub.get_collection(collection_name)
+        datasets = collection.items
+        for dataset_item in datasets:
+            print(f"\n---------------------------\nEvaluating {dataset_item.item_id}")
+            dataset = cast(Dataset, load_dataset(dataset_item.item_id, split=split))
+            metrics = evaluate_dataset(retriever, dataset, batch_query= batch_query, batch_doc=batch_doc) 
+            log_metrics(metrics, dataset_item.item_id, log_file=str(savepath))
+            print(f"Metrics saved to {savepath}")
+            
+            print(f"NDCG@5 for {model_name} on {dataset_item.item_id}: {metrics['ndcg_at_5']}")
+    else:
+        raise ValueError("Please provide a dataset name or collection name")
 
 if __name__ == "__main__":
     typer.run(main)
---
+++ src/vidore_benchmark/utils/log_utils.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import json
+
+def log_metrics(metrics, dataset_name, log_file="metrics.json"):
+    if Path(log_file).exists():
+        loaded_metrics = json.load(open(log_file))
+        loaded_metrics[dataset_name] = metrics
+        with open(log_file, "w") as f:
+            json.dump(loaded_metrics, f)
+    else:
+        with open(log_file, "w") as f:
+            json.dump({dataset_name: metrics}, f)
