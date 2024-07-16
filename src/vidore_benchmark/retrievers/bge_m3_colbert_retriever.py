386e2c473741260c79279c1594613573db19bbd8
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-07-16 15:53:54 +0200

    feat(retriever): add support for "BAAI/bge-m3-colbert" retriever (multi-vector embeddings)

--- src/vidore_benchmark/retrievers/bge_m3_colbert_retriever.py
+++ src/vidore_benchmark/retrievers/bge_m3_colbert_retriever.py
@@ -5,16 +5,17 @@ import torch
 from FlagEmbedding import BGEM3FlagModel
 from tqdm import tqdm
 
+from vidore_benchmark.evaluation.colbert_score import get_colbert_similarity
 from vidore_benchmark.retrievers.utils.register_retriever import register_vision_retriever
 from vidore_benchmark.retrievers.vision_retriever import VisionRetriever
 from vidore_benchmark.utils.iter_utils import batched
 from vidore_benchmark.utils.torch_utils import get_torch_device
 
 
-@register_vision_retriever("BAAI/bge-m3")
-class BGEM3Retriever(VisionRetriever):
+@register_vision_retriever("BAAI/bge-m3-colbert")
+class BGEM3ColbertRetriever(VisionRetriever):
     """
-    BGEM3Retriever class to retrieve embeddings the BGE-M3 model (dense embeddings).
+    BGEM3Retriever class to retrieve embeddings the BGE-M3 model (multi-vector embeddings + ColBERT scoring).
     """
 
     def __init__(self, device: str = "auto"):
@@ -28,16 +29,20 @@ class BGEM3Retriever(VisionRetriever):
     def use_visual_embedding(self) -> bool:
         return False
 
-    def forward_queries(self, queries, batch_size: int, **kwargs) -> List[torch.Tensor]:
+    def forward_queries(self, queries: List[str], batch_size: int, **kwargs) -> List[torch.Tensor]:
         list_emb_queries: List[torch.Tensor] = []
         for query_batch in tqdm(
             batched(queries, batch_size), desc="Query batch", total=math.ceil(len(queries) / batch_size)
         ):
             query_batch = cast(List[str], query_batch)
-            output = self.model.encode(query_batch, max_length=512)["dense_vecs"]
-            query_embeddings = torch.tensor(output).to(self.device)
-            list_emb_queries.append(query_embeddings)
-
+            output = self.model.encode(
+                query_batch,
+                max_length=512,
+                return_dense=False,
+                return_sparse=False,
+                return_colbert_vecs=True,
+            )["colbert_vecs"]
+            list_emb_queries.extend([torch.Tensor(elt) for elt in output])
         return list_emb_queries
 
     def forward_documents(self, documents: List[str], batch_size: int, **kwargs) -> List[torch.Tensor]:
@@ -46,9 +51,14 @@ class BGEM3Retriever(VisionRetriever):
             batched(documents, batch_size), desc="Document batch", total=math.ceil(len(documents) / batch_size)
         ):
             doc_batch = cast(List[str], doc_batch)
-            output = self.model.encode(doc_batch)["dense_vecs"]
-            doc_embeddings = torch.tensor(output).to(self.device)
-            list_emb_documents.append(doc_embeddings)
+            output = self.model.encode(
+                doc_batch,
+                max_length=512,
+                return_dense=False,
+                return_sparse=False,
+                return_colbert_vecs=True,
+            )["colbert_vecs"]
+            list_emb_documents.extend([torch.Tensor(elt) for elt in output])
         return list_emb_documents
 
     def get_scores(
@@ -56,8 +66,5 @@ class BGEM3Retriever(VisionRetriever):
         list_emb_queries: List[torch.Tensor],
         list_emb_documents: List[torch.Tensor],
     ) -> torch.Tensor:
-        emb_queries = torch.cat(list_emb_queries, dim=0)  # (num_queries, emb_dim_query)
-        emb_documents = torch.cat(list_emb_documents, dim=0)  # (num_docs, emb_dim_doc)
-
-        scores = torch.einsum("bd,cd->bc", emb_queries, emb_documents)  # (num_queries, num_docs)
+        scores = get_colbert_similarity(list_emb_queries, list_emb_documents)
         return scores
