2af0932629e04ea40bc94e8a24ed055ace71cbe0
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-07-26 16:08:30 +0200

    fix(retrievers): fix wrong scores in DummyRetriever

--- src/vidore_benchmark/retrievers/dummy_retriever.py
+++ src/vidore_benchmark/retrievers/dummy_retriever.py
@@ -44,4 +44,6 @@ class DummyRetriever(VisionRetriever):
         list_emb_documents: List[torch.Tensor],
         batch_size: Optional[int] = None,
     ) -> torch.Tensor:
-        return torch.randint(0, 1, (len(list_emb_queries), len(list_emb_documents)))
+        if batch_size is None:
+            batch_size = 1
+        return torch.rand(len(list_emb_queries) * batch_size, len(list_emb_documents) * batch_size)
