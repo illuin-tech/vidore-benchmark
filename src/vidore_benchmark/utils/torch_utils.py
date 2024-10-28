dcd4187af2b72e4fe84b6d2277774ceba1a69539
ManuelFay <manuel.faysse@illuin.tech>
2024-10-28 11:41:33 +0100

    fic

--- src/vidore_benchmark/utils/torch_utils.py
+++ src/vidore_benchmark/utils/torch_utils.py
@@ -16,7 +16,7 @@ def get_torch_device(device: str = "auto") -> str:
     """
     if device == "auto":
         if torch.cuda.is_available():
-            return "cuda:0"
+            return "cuda"
         elif torch.backends.mps.is_available():  # for Apple Silicon
             return "mps"
         else:
