6177671199a01f95eb5841aeef039e6530cb2e5c
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-10-30 10:11:27 +0100

    refactor: rename input arg for `register_vision_retriever`

--- src/vidore_benchmark/retrievers/utils/register_retriever.py
+++ src/vidore_benchmark/retrievers/utils/register_retriever.py
@@ -7,12 +7,12 @@ from vidore_benchmark.retrievers.vision_retriever import VisionRetriever
 VISION_RETRIEVER_REGISTRY: Dict[str, Type[VisionRetriever]] = {}
 
 
-def register_vision_retriever(model_name: str):
+def register_vision_retriever(model_class: str):
     def decorator(cls):
-        VISION_RETRIEVER_REGISTRY[model_name] = cls
+        VISION_RETRIEVER_REGISTRY[model_class] = cls
 
         # NOTE: To see this log, use `logger.enable("vidore_benchmark")` at the very top of the script.
-        logger.debug("Registered vision retriever `{}`", model_name)
+        logger.debug("Registered vision retriever `{}`", model_class)
 
         return cls
 
