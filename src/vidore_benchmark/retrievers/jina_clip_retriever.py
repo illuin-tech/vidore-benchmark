12589d1ca78b76dc549a38e63ab756c810e412b2
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-11-02 23:26:07 +0100

    fix: replace all `torch.cat` with `torch.stack`

--- src/vidore_benchmark/retrievers/jina_clip_retriever.py
+++ src/vidore_benchmark/retrievers/jina_clip_retriever.py
@@ -85,9 +85,9 @@ class JinaClipRetriever(VisionRetriever):
         batch_size: Optional[int] = None,
     ) -> torch.Tensor:
         if isinstance(query_embeddings, list):
-            query_embeddings = torch.cat(query_embeddings, dim=0)
+            query_embeddings = torch.stack(query_embeddings)
         if isinstance(passage_embeddings, list):
-            passage_embeddings = torch.cat(passage_embeddings, dim=0)
+            passage_embeddings = torch.stack(passage_embeddings)
 
         scores = torch.einsum("bd,cd->bc", query_embeddings, passage_embeddings)
         return scores
