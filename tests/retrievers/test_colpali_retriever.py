5449678114c71ab6c237b7ea7a2239c427b1cbad
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-11-04 21:37:33 +0100

    test: add fixtures for embeddings

--- tests/retrievers/test_colpali_retriever.py
+++ tests/retrievers/test_colpali_retriever.py
@@ -27,8 +27,10 @@ def test_forward_documents(retriever: ColPaliRetriever, image_passage_fixture):
 
 
 @pytest.mark.slow
-def test_get_scores(retriever: ColPaliRetriever, queries_fixture, image_passage_fixture):
-    query_embeddings = retriever.forward_queries(queries_fixture, batch_size=1)
-    passage_embeddings = retriever.forward_passages(image_passage_fixture, batch_size=1)
-    scores = retriever.get_scores(query_embeddings, passage_embeddings)
-    assert scores.shape == (len(queries_fixture), len(image_passage_fixture))
+def test_get_scores(
+    retriever: ColPaliRetriever,
+    query_multi_vector_embeddings_fixture,
+    passage_multi_vector_embeddings_fixture,
+):
+    scores = retriever.get_scores(query_multi_vector_embeddings_fixture, passage_multi_vector_embeddings_fixture)
+    assert scores.shape == (len(query_multi_vector_embeddings_fixture), len(passage_multi_vector_embeddings_fixture))
