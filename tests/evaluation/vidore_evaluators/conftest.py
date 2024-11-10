84cb28e7addfafcf7f80365c3ab41a75e72655f4
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-11-10 17:18:15 +0100

    test: fix `MockBM25Retriever`

--- tests/evaluation/vidore_evaluators/conftest.py
+++ tests/evaluation/vidore_evaluators/conftest.py
@@ -1,21 +1,12 @@
 import pytest
 import torch
 
-from vidore_benchmark.retrievers.base_vision_retriever import BaseVisionRetriever
+from vidore_benchmark.retrievers.bm25_retriever import BM25Retriever
 
 
-class MockBM25Retriever(BaseVisionRetriever):
+class MockBM25Retriever(BM25Retriever):
     def __init__(self, device: str = "auto"):
-        super().__init__(use_visual_embedding=False)
-
-    def forward_queries(self, queries, batch_size=None, **kwargs):
-        raise NotImplementedError("BM25Retriever only need get_scores_bm25 method.")
-
-    def forward_passages(self, passages, batch_size=None, **kwargs):
-        raise NotImplementedError("BM25Retriever only need get_scores_bm25 method.")
-
-    def get_scores(self, query_embeddings, passage_embeddings, batch_size=None):
-        raise NotImplementedError("Please use the `get_scores_bm25` method instead.")
+        super().__init__()
 
     def get_scores_bm25(self, queries, passages, **kwargs):
         return torch.tensor([[0.5 for _ in range(len(passages))] for _ in range(len(queries))])
