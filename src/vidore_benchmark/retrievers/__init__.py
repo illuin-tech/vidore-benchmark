934944febfbb20f277de093aa77200f833f6ba22
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-11-10 15:28:56 +0100

    refactor: rename `DummyRetriever` to `DummyVisionRetriever`

--- src/vidore_benchmark/retrievers/__init__.py
+++ src/vidore_benchmark/retrievers/__init__.py
@@ -8,7 +8,7 @@ from .colidefics3_retriever import ColIdefics3Retriever
 from .colpali_retriever import ColPaliRetriever
 from .colqwen2_retriever import ColQwen2Retriever
 from .dse_qwen2_retriever import DSEQwen2Retriever
-from .dummy_retriever import DummyRetriever
+from .dummy_vision_retriever import DummyVisionRetriever
 from .jina_clip_retriever import JinaClipRetriever
 from .nomic_retriever import NomicVisionRetriever
 from .registry_utils import VISION_RETRIEVER_REGISTRY, load_vision_retriever_from_registry, register_vision_retriever
--- tests/retrievers/test_dummy_vision_retriever.py
+++ tests/retrievers/test_dummy_vision_retriever.py
@@ -2,28 +2,28 @@ from typing import Generator
 
 import pytest
 
-from vidore_benchmark.retrievers.dummy_retriever import DummyRetriever
+from vidore_benchmark.retrievers.dummy_vision_retriever import DummyVisionRetriever
 from vidore_benchmark.utils.torch_utils import tear_down_torch
 
 
 @pytest.fixture(scope="module")
-def retriever() -> Generator[DummyRetriever, None, None]:
-    yield DummyRetriever()
+def retriever() -> Generator[DummyVisionRetriever, None, None]:
+    yield DummyVisionRetriever()
     tear_down_torch()
 
 
-def test_forward_queries(retriever: DummyRetriever, queries_fixture):
+def test_forward_queries(retriever: DummyVisionRetriever, queries_fixture):
     embeddings_queries = retriever.forward_queries(queries_fixture, batch_size=1)
     assert len(embeddings_queries) == len(queries_fixture)
 
 
-def test_forward_documents(retriever: DummyRetriever, image_passage_fixture):
+def test_forward_documents(retriever: DummyVisionRetriever, image_passage_fixture):
     embeddings_docs = retriever.forward_passages(image_passage_fixture, batch_size=1)
     assert len(embeddings_docs) == len(image_passage_fixture)
 
 
 def test_get_scores(
-    retriever: DummyRetriever,
+    retriever: DummyVisionRetriever,
     query_single_vector_embeddings_fixture,
     passage_single_vector_embeddings_fixture,
 ):
