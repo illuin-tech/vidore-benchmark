4facbbb0c3e07fd3b516d0ffd0fef89b5afa8e6b
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-12-07 11:29:15 +0100

    feat: remove unused `plot_utils.py`

--- src/vidore_benchmark/utils/plot_utils.py
+++
@@ -1,9 +0,0 @@
-import seaborn as sns
-
-
-def setup_plot_style():
-    """
-    Util to have a consistent style for all matplotlib plots.
-    """
-    sns.set_style("white")
-    sns.set_context("paper", font_scale=2)
