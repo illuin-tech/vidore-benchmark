dd9cf42d07bd5bbd160ecf26968e1d69d5ebaabf
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-12-17 22:03:53 +0000

    refactor: move `hash_image` to `image_utils.py`

--- src/vidore_benchmark/utils/__init__.py
+++ src/vidore_benchmark/utils/__init__.py
@@ -1,4 +1,5 @@
 from .data_utils import ListDataset
+from .image_utils import hash_image
 from .iter_utils import batched, islice
 from .logging_utils import setup_logging
 from .torch_utils import get_torch_device, tear_down_torch
