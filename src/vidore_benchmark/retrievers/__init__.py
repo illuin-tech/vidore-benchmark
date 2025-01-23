f29a2f477e0cddbbf699ce352e2fd2397c6ff196
Manuel Faysse <43467008+ManuelFay@users.noreply.github.com>
2025-01-23 10:36:40 +0100

    Add colifefics3 (#71)
    * colidefics3
    
    * add init
    
    * num workers
    
    * num workers
    
    * Update src/vidore_benchmark/retrievers/colidefics3_retriever.py
    
    * Update src/vidore_benchmark/retrievers/colidefics3_retriever.py
    
    * style: lint
    
    * chore: update CHANGELOG
    
    ---------
    
    Co-authored-by: Tony Wu <28306721+tonywu71@users.noreply.github.com>

--- src/vidore_benchmark/retrievers/__init__.py
+++ src/vidore_benchmark/retrievers/__init__.py
@@ -3,6 +3,7 @@ from .bge_m3_retriever import BGEM3Retriever
 from .biqwen2_retriever import BiQwen2Retriever
 from .bm25_retriever import BM25Retriever
 from .cohere_api_retriever import CohereAPIRetriever
+from .colidefics3_retriever import ColIdefics3Retriever
 from .colpali_retriever import ColPaliRetriever
 from .colqwen2_retriever import ColQwen2Retriever
 from .dse_qwen2_retriever import DSEQwen2Retriever
--- src/vidore_benchmark/retrievers/colidefics3_retriever.py
+++ src/vidore_benchmark/retrievers/colidefics3_retriever.py
@@ -1,82 +1,71 @@
 from __future__ import annotations
 
 import logging
-from typing import List, Optional, Union, cast
+from typing import ClassVar, List, Optional, Union
 
 import torch
+from colpali_engine.models import ColIdefics3, ColIdefics3Processor
+from colpali_engine.utils.torch_utils import get_torch_device
 from dotenv import load_dotenv
 from PIL import Image
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
+from vidore_benchmark.evaluation.scoring import score_multi_vector
 from vidore_benchmark.retrievers.registry_utils import register_vision_retriever
 from vidore_benchmark.retrievers.vision_retriever import VisionRetriever
 from vidore_benchmark.utils.data_utils import ListDataset
-from vidore_benchmark.utils.torch_utils import get_torch_device
 
 logger = logging.getLogger(__name__)
 
 load_dotenv(override=True)
 
 
-@register_vision_retriever("biqwen2")
-class BiQwen2Retriever(VisionRetriever):
+@register_vision_retriever("colidefics3")
+class ColIdefics3Retriever(VisionRetriever):
     """
-    BiQwen2 retriever that implements the model from "ColPali: Efficient Document Retrieval
+    ColIdefics3 Retriever that implements the ColVision architecture with an Idefics3 VLM backbone.
     with Vision Language Models".
     """
 
+    emb_dim_query: ClassVar[int] = 128
+    emb_dim_doc: ClassVar[int] = 128
+
     def __init__(
         self,
-        pretrained_model_name_or_path: str,
+        pretrained_model_name_or_path: str = "vidore/colidefics-v0.1",
         device: str = "auto",
         num_workers: int = 8,
     ):
         super().__init__()
 
-        try:
-            from colpali_engine.models import BiQwen2, BiQwen2Processor
-        except ImportError:
-            raise ImportError(
-                'Install the missing dependencies with `pip install "vidore-benchmark[colpali-engine]"` '
-                "to use BiQwen2Retriever."
-            )
-
         self.device = get_torch_device(device)
         logger.info(f"Using device: {self.device}")
 
         # Load the model and LORA adapter
-        self.model = cast(
-            BiQwen2,
-            BiQwen2.from_pretrained(
-                pretrained_model_name_or_path,
-                torch_dtype=torch.bfloat16,
-                device_map=device,
-                attn_implementation="flash_attention_2" if torch.cuda.is_available() else None,
-            ).eval(),
-        )
+        self.model = ColIdefics3.from_pretrained(
+            pretrained_model_name_or_path,
+            torch_dtype=torch.bfloat16,
+            device_map=device,
+            attn_implementation="flash_attention_2" if torch.cuda.is_available() else None,
+        ).eval()
 
         # Load the processor
-        self.processor = cast(BiQwen2Processor, BiQwen2Processor.from_pretrained(pretrained_model_name_or_path))
-        print("Loaded custom processor.\n")
+        self.processor = ColIdefics3Processor.from_pretrained(pretrained_model_name_or_path)
         self.num_workers = num_workers
+        print("Loaded custom processor.\n")
 
     @property
     def use_visual_embedding(self) -> bool:
         return True
 
     def process_images(self, images: List[Image.Image], **kwargs):
-        return self.processor.process_images(images=images).to(self.device)
+        return self.processor.process_images(images=images)
 
     def process_queries(self, queries: List[str], **kwargs):
-        return self.processor.process_queries(queries=queries).to(self.device)
+        return self.processor.process_queries(queries=queries)
 
-    def forward_queries(
-        self,
-        queries: List[str],
-        batch_size: int,
-        **kwargs,
-    ) -> List[torch.Tensor]:
+    def forward_queries(self, queries: List[str], batch_size: int, **kwargs) -> List[torch.Tensor]:
         dataloader = DataLoader(
             dataset=ListDataset[str](queries),
             batch_size=batch_size,
@@ -85,39 +74,31 @@ class BiQwen2Retriever(VisionRetriever):
             num_workers=self.num_workers,
         )
 
-        query_embeddings: List[torch.Tensor] = []
-
+        qs = []
         for batch_query in tqdm(dataloader, desc="Forward pass queries...", leave=False):
             with torch.no_grad():
                 batch_query = {k: v.to(self.device) for k, v in batch_query.items()}
                 embeddings_query = self.model(**batch_query)
-                query_embeddings.extend(list(torch.unbind(embeddings_query.to("cpu"))))
+                qs.extend(list(torch.unbind(embeddings_query.to("cpu"))))
 
-        return query_embeddings
+        return qs
 
-    def forward_passages(
-        self,
-        passages: List[Image.Image],
-        batch_size: int,
-        **kwargs,
-    ) -> List[torch.Tensor]:
+    def forward_passages(self, passages: List[Image.Image], batch_size: int, **kwargs) -> List[torch.Tensor]:
         dataloader = DataLoader(
             dataset=ListDataset[Image.Image](passages),
             batch_size=batch_size,
             shuffle=False,
             collate_fn=self.process_images,
-            num_workers=self.num_workers
+            num_workers=self.num_workers,
         )
 
-        passage_embeddings: List[torch.Tensor] = []
-
+        ds = []
         for batch_doc in tqdm(dataloader, desc="Forward pass documents...", leave=False):
             with torch.no_grad():
                 batch_doc = {k: v.to(self.device) for k, v in batch_doc.items()}
                 embeddings_doc = self.model(**batch_doc)
-            passage_embeddings.extend(list(torch.unbind(embeddings_doc.to("cpu"))))
-
-        return passage_embeddings
+            ds.extend(list(torch.unbind(embeddings_doc.to("cpu"))))
+        return ds
 
     def get_scores(
         self,
@@ -126,11 +107,6 @@ class BiQwen2Retriever(VisionRetriever):
         batch_size: Optional[int] = 128,
     ) -> torch.Tensor:
         if batch_size is None:
-            raise ValueError("`batch_size` must be provided for BiQwenRetriever's scoring")
-        scores = self.processor.score(
-            query_embeddings,
-            passage_embeddings,
-            batch_size=batch_size,
-            device="cpu",
-        )
+            raise ValueError("`batch_size` must be provided for ColIdefics3's scoring")
+        scores = score_multi_vector(query_embeddings, passage_embeddings, batch_size=batch_size)
         return scores
