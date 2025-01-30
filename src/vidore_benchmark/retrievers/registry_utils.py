2d2865c04e966b1ea842f91e23fdf32229481ca1
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2025-01-30 11:01:46 +0100

    docs: remove old comment

--- src/vidore_benchmark/retrievers/registry_utils.py
+++ src/vidore_benchmark/retrievers/registry_utils.py
@@ -11,10 +11,7 @@ logger = logging.getLogger(__name__)
 def register_vision_retriever(model_class: str):
     def decorator(cls):
         VISION_RETRIEVER_REGISTRY[model_class] = cls
-
-        # NOTE: To see this log, use `logger.enable("vidore_benchmark")` at the very top of the script.
         logger.debug("Registered vision retriever `{}`", model_class)
-
         return cls
 
     return decorator
