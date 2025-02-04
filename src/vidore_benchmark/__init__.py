4f6671c6a03d4b72b2bb063115e9a55ce2c1a477
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2025-02-04 11:36:14 +0100

    feat: allow user to import retrievers from root module

---
+++ src/vidore_benchmark/__init__.py
@@ -0,0 +1,17 @@
+from .retrievers import (
+    BaseVisionRetriever,
+    BGEM3ColbertRetriever,
+    BGEM3Retriever,
+    BiQwen2Retriever,
+    BM25Retriever,
+    CohereAPIRetriever,
+    ColIdefics3Retriever,
+    ColPaliRetriever,
+    ColQwen2Retriever,
+    DSEQwen2Retriever,
+    DummyVisionRetriever,
+    JinaClipRetriever,
+    NomicVisionRetriever,
+    SigLIPRetriever,
+    VisionRetriever,
+)
