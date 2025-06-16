e73672c8f9b658456941c39ee039bc71f4ae08c7
violenil <isabelle.mohr@jina.ai>
2025-06-16 11:06:56 +0200

    feat: support language selection for qa

--- src/vidore_benchmark/cli/main.py
+++ src/vidore_benchmark/cli/main.py
@@ -15,7 +15,8 @@ from vidore_benchmark.evaluation.vidore_evaluators import ViDoReEvaluatorQA
 from vidore_benchmark.evaluation.vidore_evaluators.vidore_evaluator_beir import ViDoReEvaluatorBEIR
 from vidore_benchmark.retrievers.base_vision_retriever import BaseVisionRetriever
 from vidore_benchmark.retrievers.registry_utils import load_vision_retriever_from_registry
-from vidore_benchmark.utils.data_utils import get_datasets_from_collection
+from vidore_benchmark.utils.data_utils import get_datasets_from_collection, get_configs_to_evaluate
+
 from vidore_benchmark.utils.logging_utils import setup_logging
 
 logger = logging.getLogger(__name__)
@@ -50,6 +51,7 @@ def _get_metrics_from_vidore_evaluator(
     vision_retriever: BaseVisionRetriever,
     dataset_name: str,
     dataset_format: str,
+    config: Optional[str],
     split: str,
     batch_query: int,
     batch_passage: int,
@@ -61,7 +63,11 @@ def _get_metrics_from_vidore_evaluator(
     Rooter function to get metrics from the ViDoRe evaluator depending on the dataset format.
     """
     if dataset_format.lower() == "qa":
-        ds = cast(Dataset, load_dataset(dataset_name, split=split))
+        ds = (
+            cast(Dataset, load_dataset(dataset_name, config, split=split))
+            if config
+            else cast(Dataset, load_dataset(dataset_name, split=split))
+        )
         vidore_evaluator = ViDoReEvaluatorQA(vision_retriever)
     elif dataset_format.lower() == "beir":
         ds = {
@@ -115,6 +121,7 @@ def evaluate_retriever(
         Optional[str],
         typer.Option(help="Dataset collection to use for evaluation. Can be a Hf collection id or a local dirpath."),
     ] = None,
+    languages: Annotated[Optional[str], typer.Option(help="Comma-separated list of languages.")] = None,
     split: Annotated[str, typer.Option(help="Dataset split")] = "test",
     batch_query: Annotated[int, typer.Option(help="Batch size for query embedding inference")] = 4,
     batch_passage: Annotated[int, typer.Option(help="Batch size for passages embedding inference")] = 4,
@@ -160,32 +167,46 @@ def evaluate_retriever(
     savedir_datasets = savedir_root / model_id.replace("/", "_")
     savedir_datasets.mkdir(parents=True, exist_ok=True)
 
+    language_configs = languages.split(",") if languages else []
+
     for dataset_name in tqdm(dataset_names, desc="Evaluating dataset(s)"):
         print(f"\n---------------------------\n{dataset_name}")
+        langs_to_evaluate = get_configs_to_evaluate(
+            dataset_name, language_configs
+        ) if dataset_format.lower() == "qa" else [None]
+        for config in sorted(langs_to_evaluate):
+            config_suffix = config or "default"
+            prediction_output_path = (
+                    savedir_datasets
+                    / f"{model_id}_{dataset_name}_{config_suffix}_predictions.json"
+            )
+            prediction_output_path.parent.mkdir(parents=True, exist_ok=True)
+            logger.info(f"Predictions saved to `{prediction_output_path}`")
 
-        metrics = _get_metrics_from_vidore_evaluator(
-            vision_retriever=retriever,
-            dataset_name=dataset_name,
-            dataset_format=dataset_format,
-            split=split,
-            batch_query=batch_query,
-            batch_passage=batch_passage,
-            batch_score=batch_score,
-            dataloader_prebatch_query=dataloader_prebatch_query,
-            dataloader_prebatch_passage=dataloader_prebatch_passage,
-        )
-        metrics_all.update(metrics)
+            metrics = _get_metrics_from_vidore_evaluator(
+                vision_retriever=retriever,
+                dataset_name=dataset_name,
+                dataset_format=dataset_format,
+                config=config_suffix,
+                split=split,
+                batch_query=batch_query,
+                batch_passage=batch_passage,
+                batch_score=batch_score,
+                dataloader_prebatch_query=dataloader_prebatch_query,
+                dataloader_prebatch_passage=dataloader_prebatch_passage,
+            )
+            metrics_all.update(metrics)
 
-        print(f"nDCG@5 on {dataset_name}: {metrics[dataset_name]['ndcg_at_5']}")
+            print(f"nDCG@5 on {dataset_name}: {metrics[dataset_name]['ndcg_at_5']}")
 
-        results = ViDoReBenchmarkResults(
-            metadata=MetadataModel(
-                timestamp=datetime.now(),
-                vidore_benchmark_version=version("vidore_benchmark"),
-            ),
-            metrics={dataset_name: metrics[dataset_name]},
-        )
-        results_all.append(results)
+            results = ViDoReBenchmarkResults(
+                metadata=MetadataModel(
+                    timestamp=datetime.now(),
+                    vidore_benchmark_version=version("vidore_benchmark"),
+                ),
+                metrics={f"{dataset_name} ({config_suffix})": metrics[dataset_name]},
+            )
+            results_all.append(results)
 
         sanitized_dataset_name = dataset_name.replace("/", "_")
         savepath_results = savedir_datasets / f"{sanitized_dataset_name}_metrics.json"
--- src/vidore_benchmark/utils/data_utils.py
+++ src/vidore_benchmark/utils/data_utils.py
@@ -5,6 +5,7 @@ from typing import List, TypeVar
 
 import huggingface_hub
 from datasets import Dataset as HfDataset
+from datasets import get_dataset_config_names
 from torch.utils.data import Dataset as TorchDataset
 
 T = TypeVar("T")
@@ -73,3 +74,38 @@ def get_datasets_from_collection(collection_name: str) -> List[str]:
         collection = huggingface_hub.get_collection(collection_name)
         dataset_names = [dataset_item.item_id for dataset_item in collection.items]
     return dataset_names
+
+def get_configs_to_evaluate(
+    dataset: str, language_configs: List[str], exclude_default: bool = False
+) -> List[str]:
+    """
+    Get the list of language configurations to evaluate for a given dataset. The returned list will either
+    be the intersection of the available configurations and the provided language configurations, or if there
+    is only a 'default' available config for the dataset, a list of [None] will be returned.
+
+    Args:
+        dataset: Dataset name
+        language_configs: List of language configurations to evaluate
+        exclude_default: Exclude the default configuration
+
+    Returns:
+        List of configurations to evaluate
+    """
+    try:
+        available_configs = get_dataset_config_names(dataset)
+        if available_configs == ["default"] and not exclude_default:
+            logging.info(f"Dataset has no language configs, loading default: {dataset}")
+            return [None]
+        elif language_configs:
+            configs = list(set(available_configs) & set(language_configs))
+            if not configs:
+                logging.info(
+                    f"No matching language configs for dataset {dataset}. Available: {available_configs}"
+                )
+                return []
+            return configs
+        else:
+            return available_configs
+    except Exception as e:
+        logging.error(f"Error getting configurations for dataset {dataset}: {str(e)}")
+        return []
